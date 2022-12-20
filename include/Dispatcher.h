8996c152cd66bd40e1abc30be4194a2577cc82d5
William Brent <w@williambrent.com>
2022-12-20 12:00:34 -0500

    declare Dispatcher functions virtual

--- include/Dispatcher.h
+++ include/Dispatcher.h
@@ -15,10 +15,10 @@ public:
 
     Leap::Frame frame;
 
-    void onInit (const Leap::Controller&);
-    void onConnect (const Leap::Controller&);
-    void onDisconnect (const Leap::Controller&);
-    void onExit (const Leap::Controller&);
-    void onFrame (const Leap::Controller&);
+    virtual void onInit (const Leap::Controller&);
+    virtual void onConnect (const Leap::Controller&);
+    virtual void onDisconnect (const Leap::Controller&);
+    virtual void onExit (const Leap::Controller&);
+    virtual void onFrame (const Leap::Controller&);
 
 };
